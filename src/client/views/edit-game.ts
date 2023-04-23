af11f45544752fd6e12aa5457eb14490cc949794
Nik <nrkn.com@gmail.com>
2023-04-23 12:51:13 +1200

    Update edit-game.ts

--- src/client/views/edit-game.ts
+++ src/client/views/edit-game.ts
@@ -1,6 +1,9 @@
-import { getGame } from '../store/game';
-import { createOrEdit } from './create-game';
+import { getGame } from '../store/game'
+import { createOrEdit } from './create-game'
 
+// this isn't really what we want to do - the editView is going to have
+// a bunch of things for helping to manage sections - but for now is fine
+// to just have one big ol' html editor
 export const editGameView = async ( name: string ) => {
   const game = await getGame( name )
 
